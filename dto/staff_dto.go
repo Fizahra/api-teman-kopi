e647c22d6f5fb8112633a5e00bfafa0c9c5217ac
Fizahra <fitriazzahra3@gmail.com>
2023-01-10 08:57:51 +0700

    update json id

--- dto/staff_dto.go
+++ dto/staff_dto.go
@@ -7,7 +7,7 @@ type StaffCreate struct {
 }
 
 type StaffUpdate struct {
-	ID     int    `gorm:"primaryKey"`
+	ID     int    `gorm:"primaryKey" json:"id"`
 	Nama   string `gorm:"not null" json:"nama" form:"nama" valid:"required~Your Nama is required"`
 	Posisi string `gorm:"not null" json:"posisi" form:"posisi" valid:"required~Your Posisi is required"`
 	Shift  string `gorm:"not null" json:"shift" form:"shift" valid:"required~Your Shift is required"`
--- model/staff_model.go
+++ model/staff_model.go
@@ -1,7 +1,7 @@
 package model
 
 type Staff struct {
-	ID     int    `gorm:"primaryKey"`
+	ID     int    `gorm:"primaryKey" json:"id"`
 	Nama   string `gorm:"not null" json:"nama" form:"nama" valid:"required~Your Nama is required"`
 	Posisi string `gorm:"not null" json:"posisi" form:"posisi" valid:"required~Your Posisi is required"`
 	Shift  string `gorm:"not null" json:"shift" form:"shift" valid:"required~Your Shift is required"`
